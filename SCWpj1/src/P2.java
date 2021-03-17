3d36e73d1039050586e083d492e24bbe8a19c102
data02 <data02@DESKTOP-03JUPNI>
2021-03-17 15:14:32 +0900

    두번째 깃

---
+++ SCWpj1/src/P2.java
@@ -0,0 +1,24 @@
+
+public class P2 {
+
+	public static void main(String[] args) {
+
+		char a,b,c,d,g;
+		
+		a ='A'; //65
+		System.out.println( a+"의 숫자값=  "+ (int)a);
+		
+		b ='a'; //97
+		c=(char)(b+1);
+		System.out.println( b+ "의 숫자값=  "+ (int)b);
+		System.out.println( c+ "의 숫자값=  "+ (int)c);
+		
+		d=90;
+		g=(char)( d + 1);
+		System.out.println( d+ "의 숫자값=  "+(int)d);
+		System.out.println( g+ "의 숫자값=  "+(int)g);
+		
+		
+		
+	}
+}
