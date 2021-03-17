3c23ddf623b7d9f4adfb628e8ed5ba4d043b1758
data02 <data02@DESKTOP-03JUPNI>
2021-03-17 12:45:49 +0900

    awdawd

---
+++ SCWp1pj/src/P1.java
@@ -0,0 +1,30 @@
+public class P1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+				int a = 3;
+						
+				int b = 4;
+				
+				int c = 5;
+				
+				System.out.println(a+b+c);
+				
+				
+				System.out.println(a+b+c);
+				
+				
+				
+				System.out.println(a+b+c);
+				
+				
+				System.out.println(a+b+c);
+				
+				
+				
+			}
+
+		
+
+	}
