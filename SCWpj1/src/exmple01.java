666155977f907c942a9e9dd9d55255465e3e9acb
data02 <data02@DESKTOP-03JUPNI>
2021-03-22 15:49:32 +0900

    code up 1001~1005

---
+++ SCWpj1/src/exmple01.java
@@ -0,0 +1,29 @@
+
+public class exmple01 {
+
+	public static void main(String[] args) {
+	
+		
+		
+	System.out.println("Hello");	
+		
+	
+	
+	System.out.println("Hello World");
+		
+	
+	
+	System.out.println("Hello");	
+	System.out.println("World");
+	
+	
+	
+	System.out.println("'Hello'");
+	
+	
+	
+	System.out.println("\"Hello World\"");
+
+	}
+
+}
