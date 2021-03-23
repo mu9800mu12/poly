5e69b6d0b50694ac15cf7a67283bc63903d06da9
data02 <mu9800mu12@naver.com>
2021-03-23 16:14:56 +0900

    code up 1006~1011

---
+++ SCWpj1/src/exmple02.java
@@ -0,0 +1,30 @@
+
+public class exmple02 {
+
+	public static void main(String[] args) {
+		
+
+	System.out.println("\"!@#$%^&*()\"");
+		
+	
+	
+	System.out.println("\"c:\\Download\\hello.cpp\"");	
+	
+	
+	
+	System.out.println("¦£¦¨¦¤");
+	System.out.println("¦§¦«¦©");
+	System.out.println("¦¦¦ª¦¥");
+	
+	int a = 7;
+	
+	System.out.println(a);
+	
+	char b = '¤Á';
+	
+	System.out.println(b);
+	
+
+	}
+
+}
