2482e6fcef57f69a1307fdafc570ac72a4b657c1
data02 <mu9800mu12@naver.com>
2021-03-24 16:37:48 +0900

    code up 1012~1014

---
+++ SCWpj1/src/Day03.java
@@ -0,0 +1,25 @@
+
+public class Day03 {
+
+	public static void main(String[] args) {
+		
+	float a = 123;
+	System.out.println(a);
+		
+	int b = 2;
+			
+	int c = 4;
+	
+	
+	System.out.println(b +" "+ c);
+	
+	char d = 'A';
+		
+    char e = 'B';
+    
+    System.out.println(d+e);
+    
+    
+	}
+
+}
