45288d0cc0c5268c2557fbdc94032a925b1ddb93
data02 <mu9800mu12@naver.com>
2021-03-25 17:21:21 +0900

    code up 1015~1017!

--- SCWpj1/src/Day04.java
+++ SCWpj1/src/Day04.java
@@ -2,35 +2,15 @@
 public class Day04 {
 
 	public static void main(String[] args) {
-		
-		
-		float a = 13.24f;
-		System.out.println(a);
-		
-		int b = 125;
-		
-		System.out.println(b+" "+b+" "+b);
-	
-		
-		
 
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
+		float a = 13.25f;
+
+		System.out.println(a);
+
+		int b = 125;
+
+		System.out.println(b + " " + b + " " + b);
+
 	}
 
 }
