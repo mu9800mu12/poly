51ed3da15bbdc9abe2b251640cb744dae67cc8af
data02 <data02@DESKTOP-03JUPNI>
2021-03-25 17:20:02 +0900

    code up 1015~1017

---
+++ SCWpj1/src/Day04.java
@@ -0,0 +1,36 @@
+
+public class Day04 {
+
+	public static void main(String[] args) {
+		
+		
+		float a = 13.24f;
+		System.out.println(a);
+		
+		int b = 125;
+		
+		System.out.println(b+" "+b+" "+b);
+	
+		
+		
+
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+
+}
