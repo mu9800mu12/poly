3cb139fbefac39ac95502b76de239bf2808a4e34
data02 <mu9800mu12@naver.com>
2021-03-26 15:49:21 +0900

    반복for문

---
+++ SCWpj1/src/Day05.java
@@ -0,0 +1,17 @@
+
+public class Day05 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		for(int a = 0; a<=20; a++) {
+			
+			System.out.println(a);
+			
+			
+		}
+		
+	}
+
+}
