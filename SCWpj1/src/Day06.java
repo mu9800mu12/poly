eebc9b31117a3761cdc1bf327cf72a07b105f73f
data02 <data02@DESKTOP-03JUPNI>
2021-03-29 15:52:14 +0900

    for문 + 활용

---
+++ SCWpj1/src/Day06.java
@@ -0,0 +1,21 @@
+
+public class Day06 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+	for(int b = 0; b<=20; b++) {
+			
+			System.out.println(b+b);
+	}
+		
+		
+		
+		
+		
+		
+	
+
+	}
+}
