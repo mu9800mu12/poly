aa232143c5b7290de91211826d82367ce4ef005f
data02 <mu9800mu12@naver.com>
2021-04-05 19:33:19 +0900

    3중 구구단 (10단까지)

---
+++ SCWpj1/src/Day09.java
@@ -0,0 +1,19 @@
+
+public class Day09 {
+
+	public static void main(String[] args) {
+
+
+		for(int a = 1; a<11; a++) {
+			
+			for(int b = 1; b<11; b++) {
+					
+				for(int c = 1; c<11; c++)
+				
+				System.out.println(a * b * c);
+				
+			}
+		}
+	}
+
+}
