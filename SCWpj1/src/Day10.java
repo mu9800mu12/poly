dc480743c07f1c3ec88e7fb233c1e959d06c6942
data02 <mu9800mu12@naver.com>
2021-04-06 19:47:57 +0900

    백준 포문 2번 문제풀이 '합'

---
+++ SCWpj1/src/Day10.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class Day10 {
+
+	public static void main(String[] args) {
+
+		int a;
+		int b = 0;
+		Scanner s = new Scanner(System.in);
+
+		System.out.println("원하는 숫자를 적으십시오");
+		a = s.nextInt();
+
+		for (int i = 0; i <= a; i++) {
+			
+			
+		b+=i;	
+			
+		}
+			
+			System.out.println(b);
+
+		
+	}
+
+}
