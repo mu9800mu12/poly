9d90a9c171a790ab89c29468e194c8cb1af7ffbc
data02 <mu9800mu12@naver.com>
2021-04-08 20:33:34 +0900

    백준 N찍기

---
+++ SCWpj1/src/Day13.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class Day13 {
+
+	public static void main(String[] args) {
+		
+		int a, b;
+		
+		
+		Scanner s = new Scanner(System.in);
+		
+		b = s.nextInt();
+		
+		for(a=1;a<=b;a++) {
+			System.out.println(a);
+		}
+		
+		
+		
+		
+		
+		
+		
+
+	}
+
+}
