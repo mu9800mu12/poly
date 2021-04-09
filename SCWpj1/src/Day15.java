ad72bf27b7e05ec72a03b0d45aee12541224bbf3
data02 <mu9800mu12@naver.com>
2021-04-09 21:00:38 +0900

    역별짓기

---
+++ SCWpj1/src/Day15.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class Day15 {
+
+	public static void main(String[] args) {
+
+		int a, b, k;
+
+		Scanner s = new Scanner(System.in);
+		
+		k = s.nextInt();
+
+		for (a = 1; a <=k; a++) { 
+			for (b = k; b >0 ; b--) {
+				
+				if (a<b) {
+					System.out.print(" ");
+				}else {
+					System.out.print("*");
+				}
+			}
+			System.out.println();
+		}
+	}
+}
